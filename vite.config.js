30662c5a20e69bb4a323261dfa3373e40846397f
Samuel <60421298+iamsamuelhere@users.noreply.github.com>
2023-06-11 17:06:17 +0530

    Update and rename vite.config.js to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-  alias: [{ find: "@", replacement: "/src" }],
-}
+  build: {
+    rollupOptions: {
+      external: 'NonExistingPath'
+    }
+  }
 })
